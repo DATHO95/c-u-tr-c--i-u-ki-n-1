98a6aa8dd81089749760feb3e49dda7f66fb1532
DATHO95 <hothanhdatc20@gmail.com>
2023-11-14 10:57:40 +0700

    bổ sung bài 5

---
+++ bai5.js
@@ -0,0 +1,12 @@
+let a = +prompt();
+
+if (a >= 8) {
+    alert("Loại giỏi")
+} else if (a >5) {
+    alert("Loại khá")
+} else if (a > 3) {
+    alert("Loại trung bình")
+}
+else {
+    alert("Ở lại lớp")
+}
